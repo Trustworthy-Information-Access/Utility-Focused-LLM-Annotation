a7c36effea19d66a43a07a096a6fe0ddc10c8e12
HengranZhang <54845883+hengran@users.noreply.github.com>
2025-09-16 16:52:46 +0800

    Update utility.sh

--- utility-annotation-code/utility.sh
+++ utility-annotation-code/utility.sh
@@ -6,4 +6,3 @@ output_dir="/root/paddlejob/workspace/env_run/output/dpr-nq/results/annotation_r
 mkdir $output_dir
 train_group_size=16
 python llm-label-utility.py --model_name_or_path $model_name_or_path --dataset_path $dataset_path --train_group_size $train_group_size --batch_size 4096 --output_dir $output_dir
-CUDA_VISIBLE_DEVICES=0,1,2,3,4,5,6,7 python /root/paddlejob/workspace/env_run/run.py --size 40000 --gpus 8 --interval 0.0
